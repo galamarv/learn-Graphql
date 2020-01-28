362310cf14c5c22b9f25e244f334a59d422f2495
Fattah Rizki Hizama L <fattah.rizki@gmail.com>
2020-01-28 16:34:17 +0700

    custom typer array graphql

--- graphql-basic/src/index.js
+++ graphql-basic/src/index.js
@@ -1,12 +1,57 @@
-import {GraphQLServer} from 'graphql-yoga';
+import {
+    GraphQLServer
+} from 'graphql-yoga';
 
 // Type definitions (schema)
 
+// Demo user data 
+const users = [{
+    id: '1',
+    name: 'Satu',
+    email: 'satu@mail.com',
+    age: 24
+}, {
+    id: '2',
+    name: 'Sadu',
+    email: 'dua@mail.com',
+    age: 22
+}, {
+    id: '3',
+    name: 'tiga',
+    email: 'tiga@mail.com',
+    age: 20
+}];
+
+const posts = [{
+    id: '1',
+    title: 'judul',
+    body: 'ini tulisan',
+    published: true
+}, {
+    id: '2',
+    title: 'bukan judul',
+    body: 'itu text',
+    published: false
+}, {
+    id: '3',
+    title: 'apakah ini judul',
+    body: 'qwerty asdfg',
+    published: true
+}];
+
+
 const typeDefs = `
     type Query {
-       post: Post!
-       add(a: Float!, b: Float!): Float!
-       adds(numbers: [Float!]!): Float!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        me: User!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
     }
 
     type Post {
@@ -21,27 +66,27 @@ const typeDefs = `
 
 const resolvers = {
     Query: {
-        add(parent, args, ctx, info) {
-            //return `${args.a} + ${args.b} = ${args.a + args.b}`
-            return args.a + args.b
-        },
-        adds(parent, args, ctx, info) {
-            if (args.numbers.length === 0){
-                return 0
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users
             } else {
-                return args.numbers.reduce((accumulator, currentValue) => {
-                    return accumulator + currentValue
+                return users.filter((user) => {
+                    return user.name.toLowerCase().includes(args.query.toLowerCase())
                 })
             }
-        }, 
-        post() {
-            return {
-                id: '123asd',
-                title: 'judul',
-                body: 'isi',
-                published: true
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts
+            } else {
+                return posts.filter((post) => {
+                    const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+                    const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+                    return isTitleMatch || isBodyMatch
+                })
             }
         }
+
     }
 }
 
